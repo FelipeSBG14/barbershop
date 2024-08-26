7b8379bc547b070c6cac7be1ca27c8278780e70b
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2024-08-25 23:22:34 -0400

    tela de login

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,7 +1,9 @@
 import 'package:asyncstate/asyncstate.dart';
+import 'package:dw_barbershop/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:dw_barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_lodaer.dart';
 import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
+import 'package:dw_barbershop/src/features/auth/register/user_register_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +19,15 @@ class BarbershopApp extends StatelessWidget {
           theme: BarbershopTheme.themeData,
           title: 'DW Barbershop',
           navigatorObservers: [asyncNavigatorObserver],
+          navigatorKey: BarbershopNavGlobalKey.instance.navKey,
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (_) => const LoginPage(),
             '/home/adm': (_) => const Text('Adm'),
+            '/auth/register/barbershop': (_) =>
+                const Center(child: Text('Barbershop Page')),
             '/home/employee': (_) => const Text('Employee'),
+            '/auth/register/user': (_) => const UserRegisterPage(),
           },
         );
       },
